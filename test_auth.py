331005a7b13cb0867a239d9d1dad43aa5543301d
Vitaly Bushlanov <144945733+snegov69@users.noreply.github.com>
2025-05-15 18:30:06 +0500

    installed packages

--- test_auth.py
+++ test_auth.py
@@ -9,47 +9,37 @@ import unittest, time, re
 
 class TestAuthorisation(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver')
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
+        self.wd = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver')
+        self.wd.implicitly_wait(30)
+
     def test_authorisation(self):
-        driver = self.driver
-        driver.get("https://qa.price-port.ru/")
-        driver.find_element_by_css_selector("a.button.button_accent.button-icon.header__button-personal > svg > path").click()
-        driver.get("https://partner.qa.price-port.ru/sign-in")
-        driver.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/form/div/div/label").click()
-        driver.find_element_by_id(":R1svf9tkq:-form-item").clear()
-        driver.find_element_by_id(":R1svf9tkq:-form-item").send_keys("v.bushlanov@thehead.ru")
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.get("https://partner.qa.price-port.ru/")
+        wd = self.wd
+        wd.get("https://qa.price-port.ru/")
+        wd.find_element_by_css_selector("a.button.button_accent.button-icon.header__button-personal > svg > path").click()
+        wd.get("https://partner.qa.price-port.ru/sign-in")
+        wd.find_element_by_xpath("//div[@id='root']/div/div/div[2]/div/form/div/div/label").click()
+        wd.find_element_by_id(":R1svf9tkq:-form-item").clear()
+        wd.find_element_by_id(":R1svf9tkq:-form-item").send_keys("v.bushlanov@thehead.ru")
+        wd.find_element_by_xpath("//button[@type='submit']").click()
+        wd.get("https://partner.qa.price-port.ru/")
     
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
+
+
+#     python3.12 -m venv /Users/v.bushlanov//Users/v.bushlanov/Documents/GitHub/python_for_testing
+#     source /Users/v.bushlanov/Python/bin/activate
+#     python3.12 -m pip install requests
